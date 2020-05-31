15d7587f5d53ad4cb6490513754e8ce69c7066e3
Thomas <thomas.guillaume@efrei.net>
2020-05-31 21:16:05 +0200

    update MDL folder => add scripts

---
+++ MLD/Data/user_app.sql
@@ -0,0 +1,11 @@
+insert into USER_APP (user_id, email, password, patient_id) values (1, 'ocasarili0@forbes.com', 'jo9XqYOkuHO', 1);
+insert into USER_APP (user_id, email, password, patient_id) values (2, 'xkezar1@cafepress.com', 'RbfjkKUNtH', 2);
+insert into USER_APP (user_id, email, password, patient_id) values (3, 'cfairpo2@shop-pro.jp', 'nNO33SPhVDX', 3);
+insert into USER_APP (user_id, email, password, patient_id) values (4, 'cnorwell3@dmoz.org', 'IdWxtvSnFGR4', 4);
+insert into USER_APP (user_id, email, password, patient_id) values (5, 'amckain4@theatlantic.com', 'b9WZ0P', 5);
+insert into USER_APP (user_id, email, password, patient_id) values (6, 'sspritt5@intel.com', 'F4Eo60M4W', 6);
+insert into USER_APP (user_id, email, password, patient_id) values (7, 'ttomczak6@diigo.com', '61NCuH09', 7);
+insert into USER_APP (user_id, email, password, patient_id) values (8, 'aaingel7@telegraph.co.uk', 'MX08yb3dLV', 8);
+insert into USER_APP (user_id, email, password, patient_id) values (9, 'hbulgen8@dell.com', 'xTILZxh', 9);
+insert into USER_APP (user_id, email, password, patient_id) values (10, 'sshellsheere9@reddit.com', 'vrlQnjv', 10);
+commit;
