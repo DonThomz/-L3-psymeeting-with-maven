b656ac06b3fe471850d57cc0f9f2298d84e7f5fc
Thomas <thomas.guillaume@efrei.net>
2020-05-31 19:38:06 +0200

    add MDL folder

---
+++ MLD/init_db_oracle.sql
@@ -0,0 +1,142 @@
+/*
+ * Copyright (c) 2020. Thomas GUILLAUME & Gabriel DUGNY
+ */
+
+---------------------------------------------------------------
+--        Script Oracle.
+---------------------------------------------------------------
+
+
+------------------------------------------------------------
+-- Table: patient
+------------------------------------------------------------
+CREATE TABLE patient(
+                        patient_id     NUMBER(10,0)  NOT NULL  ,
+                        name           VARCHAR2 (50) NOT NULL  ,
+                        last_name      VARCHAR2 (50) NOT NULL  ,
+                        birthday       DATE   ,
+                        gender         VARCHAR2 (50)  ,
+                        relationship   VARCHAR2 (50)  ,
+                        discovery_way  VARCHAR2 (50)  ,
+                        CONSTRAINT patient_PK PRIMARY KEY (patient_id)
+);
+
+------------------------------------------------------------
+-- Table: jobs
+------------------------------------------------------------
+CREATE TABLE jobs(
+                     jobs_id   NUMBER(10,0)  NOT NULL  ,
+                     job_name  VARCHAR2 (50) NOT NULL  ,
+                     CONSTRAINT jobs_PK PRIMARY KEY (jobs_id)
+);
+
+------------------------------------------------------------
+-- Table: consultation
+------------------------------------------------------------
+CREATE TABLE consultation(
+                             consultation_id    NUMBER(10,0)  NOT NULL  ,
+                             consultation_date  DATE  NOT NULL  ,
+                             price              FLOAT   ,
+                             pay_mode           VARCHAR2 (20)  ,
+                             anxiety            NUMBER (1) NOT NULL  ,
+                             couple             NUMBER (1) NOT NULL  ,
+                             CONSTRAINT consultation_PK PRIMARY KEY (consultation_id),
+                             CONSTRAINT CHK_BOOLEAN_anxiety CHECK (anxiety IN (0,1))
+);
+
+------------------------------------------------------------
+-- Table: feedback
+------------------------------------------------------------
+CREATE TABLE feedback(
+                         feedback_id      NUMBER(10,0)  NOT NULL  ,
+                         indicator        NUMBER(10,0)   ,
+                         consultation_id  NUMBER(10,0)  NOT NULL  ,
+                         CONSTRAINT feedback_PK PRIMARY KEY (feedback_id)
+
+    ,CONSTRAINT feedback_consultation_FK FOREIGN KEY (consultation_id) REFERENCES consultation(consultation_id)
+    ,CONSTRAINT feedback_consultation_AK UNIQUE (consultation_id)
+);
+
+------------------------------------------------------------
+-- Table: user_app
+------------------------------------------------------------
+CREATE TABLE user_app(
+                         user_id     NUMBER(10,0)  NOT NULL  ,
+                         email       VARCHAR2 (50) NOT NULL  ,
+                         password    VARCHAR2 (50) NOT NULL  ,
+                         patient_id  NUMBER(10,0)  NOT NULL  ,
+                         CONSTRAINT user_app_PK PRIMARY KEY (user_id)
+
+    ,CONSTRAINT user_app_patient_FK FOREIGN KEY (patient_id) REFERENCES patient(patient_id)
+    ,CONSTRAINT user_app_patient_AK UNIQUE (patient_id)
+);
+
+------------------------------------------------------------
+-- Table: keyword
+------------------------------------------------------------
+CREATE TABLE keyword(
+                        keyword_id   NUMBER(10,0)  NOT NULL  ,
+                        keyword      CLOB  NOT NULL  ,
+                        feedback_id  NUMBER(10,0)  NOT NULL  ,
+                        CONSTRAINT keyword_PK PRIMARY KEY (keyword_id)
+
+    ,CONSTRAINT keyword_feedback_FK FOREIGN KEY (feedback_id) REFERENCES feedback(feedback_id)
+);
+
+------------------------------------------------------------
+-- Table: commentary
+------------------------------------------------------------
+CREATE TABLE commentary(
+                           commentary_id  NUMBER(10,0)  NOT NULL  ,
+                           commentary     CLOB  NOT NULL  ,
+                           feedback_id    NUMBER(10,0)  NOT NULL  ,
+                           CONSTRAINT commentary_PK PRIMARY KEY (commentary_id)
+
+    ,CONSTRAINT commentary_feedback_FK FOREIGN KEY (feedback_id) REFERENCES feedback(feedback_id)
+);
+
+------------------------------------------------------------
+-- Table: posture
+------------------------------------------------------------
+CREATE TABLE posture(
+                        posture_id   NUMBER(10,0)  NOT NULL  ,
+                        posture      CLOB  NOT NULL  ,
+                        feedback_id  NUMBER(10,0)  NOT NULL  ,
+                        CONSTRAINT posture_PK PRIMARY KEY (posture_id)
+
+    ,CONSTRAINT posture_feedback_FK FOREIGN KEY (feedback_id) REFERENCES feedback(feedback_id)
+);
+
+------------------------------------------------------------
+-- Table: patientjob
+------------------------------------------------------------
+CREATE TABLE patientjob(
+                           jobs_id     NUMBER(10,0)  NOT NULL  ,
+                           patient_id  NUMBER(10,0)  NOT NULL  ,
+                           job_date    DATE  NOT NULL  ,
+                           CONSTRAINT patientjob_PK PRIMARY KEY (jobs_id,patient_id)
+
+    ,CONSTRAINT patientjob_jobs_FK FOREIGN KEY (jobs_id) REFERENCES jobs(jobs_id)
+    ,CONSTRAINT patientjob_patient0_FK FOREIGN KEY (patient_id) REFERENCES patient(patient_id)
+);
+
+------------------------------------------------------------
+-- Table: consultation_carryout
+------------------------------------------------------------
+CREATE TABLE consultation_carryout(
+                                      patient_id       NUMBER(10,0)  NOT NULL  ,
+                                      consultation_id  NUMBER(10,0)  NOT NULL  ,
+                                      CONSTRAINT consultation_carryout_PK PRIMARY KEY (patient_id,consultation_id)
+
+    ,CONSTRAINT consultation_carryout_patient_FK FOREIGN KEY (patient_id) REFERENCES patient(patient_id)
+    ,CONSTRAINT consultation_carryout_consultation0_FK FOREIGN KEY (consultation_id) REFERENCES consultation(consultation_id)
+);
+
+
+commit;
+
+
+
+
+
+
---
+++ MLD/init_db_postgresql.sql
@@ -0,0 +1,143 @@
+------------------------------------------------------------
+--        Script Postgre
+------------------------------------------------------------
+
+
+
+------------------------------------------------------------
+-- Table: patient
+------------------------------------------------------------
+CREATE TABLE public.patient(
+	patient_id      INT  NOT NULL ,
+	name            VARCHAR (50) NOT NULL ,
+	last_name       VARCHAR (50) NOT NULL ,
+	birthday        DATE   ,
+	gender          VARCHAR (50)  ,
+	relationship    VARCHAR (50)  ,
+	discovery_way   VARCHAR (50)   ,
+	CONSTRAINT patient_PK PRIMARY KEY (patient_id)
+)WITHOUT OIDS;
+
+
+------------------------------------------------------------
+-- Table: jobs
+------------------------------------------------------------
+CREATE TABLE public.jobs(
+	jobs_id    INT  NOT NULL ,
+	job_name   VARCHAR (50) NOT NULL  ,
+	CONSTRAINT jobs_PK PRIMARY KEY (jobs_id)
+)WITHOUT OIDS;
+
+
+------------------------------------------------------------
+-- Table: consultation
+------------------------------------------------------------
+CREATE TABLE public.consultation(
+	consultation_id     INT  NOT NULL ,
+	consultation_date   DATE  NOT NULL ,
+	price               FLOAT   ,
+	pay_mode            VARCHAR (20)  ,
+	anxiety             BOOL  NOT NULL ,
+	couple              BOOL  NOT NULL  ,
+	CONSTRAINT consultation_PK PRIMARY KEY (consultation_id)
+)WITHOUT OIDS;
+
+
+------------------------------------------------------------
+-- Table: feedback
+------------------------------------------------------------
+CREATE TABLE public.feedback(
+	feedback_id       INT  NOT NULL ,
+	indicator         INT   ,
+	consultation_id   INT  NOT NULL  ,
+	CONSTRAINT feedback_PK PRIMARY KEY (feedback_id)
+
+	,CONSTRAINT feedback_consultation_FK FOREIGN KEY (consultation_id) REFERENCES public.consultation(consultation_id)
+	,CONSTRAINT feedback_consultation_AK UNIQUE (consultation_id)
+)WITHOUT OIDS;
+
+
+------------------------------------------------------------
+-- Table: user_app
+------------------------------------------------------------
+CREATE TABLE public.user_app(
+	user_id      INT  NOT NULL ,
+	email        VARCHAR (50) NOT NULL ,
+	password     VARCHAR (50) NOT NULL ,
+	patient_id   INT  NOT NULL  ,
+	CONSTRAINT user_app_PK PRIMARY KEY (user_id)
+
+	,CONSTRAINT user_app_patient_FK FOREIGN KEY (patient_id) REFERENCES public.patient(patient_id)
+	,CONSTRAINT user_app_patient_AK UNIQUE (patient_id)
+)WITHOUT OIDS;
+/*
+ * Copyright (c) 2020. Thomas GUILLAUME & Gabriel DUGNY
+ */
+
+------------------------------------------------------------
+-- Table: keyword
+------------------------------------------------------------
+CREATE TABLE public.keyword(
+	keyword_id    INT  NOT NULL ,
+	keyword       VARCHAR (2000)  NOT NULL ,
+	feedback_id   INT  NOT NULL  ,
+	CONSTRAINT keyword_PK PRIMARY KEY (keyword_id)
+
+	,CONSTRAINT keyword_feedback_FK FOREIGN KEY (feedback_id) REFERENCES public.feedback(feedback_id)
+)WITHOUT OIDS;
+
+
+------------------------------------------------------------
+-- Table: commentary
+------------------------------------------------------------
+CREATE TABLE public.commentary(
+	commentary_id   INT  NOT NULL ,
+	commentary      VARCHAR (2000)  NOT NULL ,
+	feedback_id     INT  NOT NULL  ,
+	CONSTRAINT commentary_PK PRIMARY KEY (commentary_id)
+
+	,CONSTRAINT commentary_feedback_FK FOREIGN KEY (feedback_id) REFERENCES public.feedback(feedback_id)
+)WITHOUT OIDS;
+
+
+------------------------------------------------------------
+-- Table: posture
+------------------------------------------------------------
+CREATE TABLE public.posture(
+	posture_id    INT  NOT NULL ,
+	posture       VARCHAR (2000)  NOT NULL ,
+	feedback_id   INT  NOT NULL  ,
+	CONSTRAINT posture_PK PRIMARY KEY (posture_id)
+
+	,CONSTRAINT posture_feedback_FK FOREIGN KEY (feedback_id) REFERENCES public.feedback(feedback_id)
+)WITHOUT OIDS;
+
+
+------------------------------------------------------------
+-- Table: patientjob
+------------------------------------------------------------
+CREATE TABLE public.patientjob(
+	jobs_id      INT  NOT NULL ,
+	patient_id   INT  NOT NULL ,
+	job_date     DATE  NOT NULL  ,
+	CONSTRAINT patientjob_PK PRIMARY KEY (jobs_id,patient_id)
+
+	,CONSTRAINT patientjob_jobs_FK FOREIGN KEY (jobs_id) REFERENCES public.jobs(jobs_id)
+	,CONSTRAINT patientjob_patient0_FK FOREIGN KEY (patient_id) REFERENCES public.patient(patient_id)
+)WITHOUT OIDS;
+
+
+------------------------------------------------------------
+-- Table: consultation_carryout
+------------------------------------------------------------
+CREATE TABLE public.consultation_carryout(
+	patient_id        INT  NOT NULL ,
+	consultation_id   INT  NOT NULL  ,
+	CONSTRAINT consultation_carryout_PK PRIMARY KEY (patient_id,consultation_id)
+
+	,CONSTRAINT consultation_carryout_patient_FK FOREIGN KEY (patient_id) REFERENCES public.patient(patient_id)
+	,CONSTRAINT consultation_carryout_consultation0_FK FOREIGN KEY (consultation_id) REFERENCES public.consultation(consultation_id)
+)WITHOUT OIDS;
+
+
+
